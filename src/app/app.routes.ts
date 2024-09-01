a1567b437e1f9181595815dfe1a868c20e5ba73a
semayank <setiyamayank30@gmail.com>
2024-09-01 13:27:59 +0530

    project initialized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"dashboard",
+        loadComponent:()=>import('./components/dashboard/dashboard.component').then((com)=>com.DashboardComponent)
+    },
+    {
+        path:'signup',
+        loadComponent:()=>import('./components/signup/signup.component').then((com)=>com.SignupComponent)
+    },
+    {
+        path:'',
+        loadComponent:()=>import('./components/signup/signup.component').then((com)=>com.SignupComponent)
+
+    },
+    {
+        path:'**',
+        loadComponent:()=>import('./components/signup/signup.component').then((com)=>com.SignupComponent)
+    }
+];
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production:false,
+    appname:'ng_gallery1',
+    supabase:{
+    url:'https://vosuxvmaehnnbiikrgjh.supabase.co',
+    key:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvc3V4dm1hZWhubmJpaWtyZ2poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUxNzEzODcsImV4cCI6MjA0MDc0NzM4N30.EdKfXzcs3JBAOaDpVNabp_OmXPWoTO7dp0zeZyHwrGM'
+    }
+};
